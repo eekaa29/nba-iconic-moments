ffe94914eca2a5c3cfd04e6cef535d3ca9cfe9a0
Ekaitz Martin <ekamartinc2003@example.com>
2025-06-30 17:18:02 +0200

    Final touches

--- theme/tailwind.config.js
+++ theme/tailwind.config.js
@@ -1,26 +1,21 @@
 module.exports = {
     content: [
-      '../core/templates/core/base.html',  // ajusta si tus templates están en otra app
-      '../core/templates/core/home.html',
-      '../core/templates/core/about.html',
-      '../core/templates/core/contact.html',
-      '../posts/templates/posts/post_list.html',
-      '../posts/templates/posts/post_update_form.html',
-      '../posts/templates/posts/create_form.html',
-      '../posts/templates/posts/post_confirm_delete.html',
-      '../article/templates/article/latest_articles.html',
-      '../registration/templates/registration/login.html',
-      '../registration/templates/registration/signup.html',
-      '../registration/templates/registration/password_reset_form.html',
-      '../registration/templates/registration/password_reset_done.html',
-      '../registration/templates/registration/password_reset_confirm.html',
-      '../registration/templates/registration/password_reset_complete.html',
-      '../registration/templates/registration/profile_form.html',
-      '../franchise/templates/franchise/franchise_list.html',        // si tienes templates en theme/
+      './core/templates/**/*.html',
+      './posts/templates/**/*.html',
+      './article/templates/**/*.html',
+      './registration/templates/**/*.html',
+      './franchise/templates/**/*.html',        // si tienes templates en theme/
     ],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          barlow: ['Barlow', 'sans-serif'],
+          osw: ['Oswald', 'sans-serif']
+        },
+      },
     },
+
+    
     plugins: [],
   }
   
